631fcee32671de1f27ba074e9611c7873e962f11
Warren Smith <warren@smith4.net>
2017-11-18 18:14:12 -0600

    Add ui skin and button
    added new UI skin and a play button on main menu.

--- core/src/com/github/t7/galacticfoundations/activities/MainActivity.java
+++ core/src/com/github/t7/galacticfoundations/activities/MainActivity.java
@@ -1,7 +1,13 @@
 package com.github.t7.galacticfoundations.activities;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.github.t7.galacticfoundations.galacticfoundations;
 
 /**
@@ -9,13 +15,30 @@ import com.github.t7.galacticfoundations.galacticfoundations;
  */
 
 public class MainActivity extends Activity {
-    Texture bg;
+    private Texture bg;
+    private TextureAtlas atlas;
+    private Skin skin;
+    private Stage stage;
 
     public MainActivity(ActivityManager activityManager){
         super(activityManager);
         viewport.apply();
         cam.setToOrtho(false, galacticfoundations.WIDTH, galacticfoundations.HEIGHT);
         bg = new Texture("badlogic.jpg");
+        atlas = new TextureAtlas("skin\\quantum-horizon-ui.atlas");
+        skin = new Skin(Gdx.files.internal("skin\\quantum-horizon-ui.json"), atlas);
+        stage = new Stage(viewport);
+        Gdx.input.setInputProcessor(stage);
+
+        Table mainTable = new Table();
+        mainTable.setFillParent(true);
+        mainTable.top();
+        TextButton playButton = new TextButton("Play", skin);
+        
+
+        mainTable.add(playButton);
+        stage.addActor(mainTable);
+
     }
 
     @Override
@@ -35,12 +58,16 @@ public class MainActivity extends Activity {
         sb.begin();
         sb.draw(bg, 0, 0);
         sb.end();
+        stage.act();
+        stage.draw();
 
     }
 
     @Override
     public void dispose() {
         bg.dispose();
+        skin.dispose();
+        atlas.dispose();
 
     }
 
