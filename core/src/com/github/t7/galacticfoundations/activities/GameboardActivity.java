e7bcabacaef6e278180a31c17d8c8792c943825a
Warren Smith <warren@smith4.net>
2017-11-19 15:10:47 -0600

    added zoom and panning on gameboard

--- core/src/com/github/t7/galacticfoundations/activities/GameboardActivity.java
+++ core/src/com/github/t7/galacticfoundations/activities/GameboardActivity.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -19,7 +20,7 @@ public class GameboardActivity extends Activity {
     private Texture bg;
     private float zoomScale;
     private Stage stage;
-    private GestureDetector.GestureListener gestureListener;
+    private GestureDetector gestureDetector;
     private InputListener inputListener;
 
 
@@ -30,64 +31,201 @@ public class GameboardActivity extends Activity {
         cam.setToOrtho(false, galacticfoundations.WIDTH, galacticfoundations.HEIGHT);
         zoomScale = 1f;
         stage = new Stage();
-        gestureListener = new GestureDetector.GestureListener() {
-            @Override
-            public boolean touchDown(float x, float y, int pointer, int button) {
-                return false;
-            }
-
-            @Override
-            public boolean tap(float x, float y, int count, int button) {
-                return false;
-            }
-
-            @Override
-            public boolean longPress(float x, float y) {
-                return false;
-            }
-
-            @Override
-            public boolean fling(float velocityX, float velocityY, int button) {
-                return false;
-            }
-
+//        gestureListener = new GestureDetector.GestureListener() {
+//            @Override
+//            public boolean touchDown(float x, float y, int pointer, int button) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean tap(float x, float y, int count, int button) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean longPress(float x, float y) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean fling(float velocityX, float velocityY, int button) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean pan(float x, float y, float deltaX, float deltaY) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean panStop(float x, float y, int pointer, int button) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean zoom(float initialDistance, float distance) {
+//                //float amount = (initialDistance - distance)/100;
+//                float amount = distance / 100;
+//                System.out.printf("Pinch level: %f\n", distance);
+//                if(cam.zoom > 0.5 && cam.zoom < 1){
+//                    cam.zoom = (float)(0.05*(amount));
+//                }
+//                else{
+//                    if(cam.zoom >= 1f){
+//                        if(amount < 0){
+//                            cam.zoom += 0.05*amount;
+//                        }
+//                    }
+//                    if(cam.zoom <= 0.5){
+//                        if(amount > 0){
+//                            cam.zoom += 0.05*amount;
+//                        }
+//                    }
+//                }
+//
+//
+//                return true;
+//            }
+//
+//            @Override
+//            public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+//                return false;
+//            }
+//
+//            @Override
+//            public void pinchStop() {
+//
+//
+//            }
+//        };
+        GestureDetector gestureDetector = new GestureDetector(new GestureDetector.GestureAdapter(){
+            private Vector2 oldInitialFirstPointer=null, oldInitialSecondPointer=null;
+            private float oldScale;
             @Override
             public boolean pan(float x, float y, float deltaX, float deltaY) {
-                return false;
-            }
+                cam.update();
+                System.out.printf("Cam X: %f, Cam Y: %f\n", cam.position.x, cam.position.y);
+                if((cam.position.x > 0 && cam.position.x < 500) && cam.position.y > 0 && cam.position.y < 500) {
+                    cam.position.add(
+                            cam.unproject(new Vector3(0, 0, 0))
+                                    .add(cam.unproject(new Vector3(deltaX, deltaY, 0)).scl(-1f))
+                    );
+                }
+                else if((cam.position.x <= 0 && cam.position.y <= 0) || (cam.position.x <= 0 && cam.position.y >= 500) || (cam.position.x >= 500 && cam.position.y <= 0) || (cam.position.x >= 500 && cam.position.y >= 500)){
+                    if(deltaX < 0 && deltaY > 0) {
+                        cam.position.add(
+                                cam.unproject(new Vector3(0, 0, 0))
+                                        .add(cam.unproject(new Vector3(deltaX, deltaY, 0)).scl(-1f))
+                        );
+                    }
+                    if(deltaX < 0 && deltaY < 0){
+                        cam.position.add(
+                                cam.unproject(new Vector3(0, 0, 0))
+                                        .add(cam.unproject(new Vector3(deltaX, deltaY, 0)).scl(-1f))
+                        );
+                    }
+                    if(deltaX > 0 && deltaY > 0){
+                        cam.position.add(
+                                cam.unproject(new Vector3(0, 0, 0))
+                                        .add(cam.unproject(new Vector3(deltaX, deltaY, 0)).scl(-1f))
+                        );
+                    }
+                    if(deltaX > 0 && deltaY < 0){
+                        cam.position.add(
+                                cam.unproject(new Vector3(0, 0, 0))
+                                        .add(cam.unproject(new Vector3(deltaX, deltaY, 0)).scl(-1f))
+                        );
+                    }
 
-            @Override
-            public boolean panStop(float x, float y, int pointer, int button) {
-                return false;
-            }
-
-            @Override
-            public boolean zoom(float initialDistance, float distance) {
-                if(zoomScale > 0.25 || zoomScale < 1){
-                    zoomScale = zoomScale + ((distance - initialDistance)/100);
-                    cam.zoom += (distance - initialDistance);
+                }
+                else{
+                    if(cam.position.x <= 0){
+                        if(deltaX < 0){
+                            cam.position.add(
+                                    cam.unproject(new Vector3(0, 0, 0))
+                                            .add(cam.unproject(new Vector3(deltaX, deltaY, 0)).scl(-1f))
+                            );
+                        } else{
+                            cam.position.add(
+                                    cam.unproject(new Vector3(0, 0, 0))
+                                            .add(cam.unproject(new Vector3(0, deltaY, 0)).scl(-1f))
+                            );
+                        }
+                    }
+                    if(cam.position.x >= 500){
+                        if(deltaX > 0){
+                            cam.position.add(
+                                    cam.unproject(new Vector3(0, 0, 0))
+                                            .add(cam.unproject(new Vector3(deltaX, deltaY, 0)).scl(-1f))
+                            );
+                        } else{
+                            cam.position.add(
+                                    cam.unproject(new Vector3(0, 0, 0))
+                                            .add(cam.unproject(new Vector3(0, deltaY, 0)).scl(-1f))
+                            );
+                        }
+                    }
+                    if(cam.position.y <= 0){
+                        if(deltaY > 0){
+                            cam.position.add(
+                                    cam.unproject(new Vector3(0, 0, 0))
+                                            .add(cam.unproject(new Vector3(deltaX, deltaY, 0)).scl(-1f))
+                            );
+                        } else{
+                            cam.position.add(
+                                    cam.unproject(new Vector3(0, 0, 0))
+                                            .add(cam.unproject(new Vector3(deltaX, 0, 0)).scl(-1f))
+                            );
+                        }
+                    }
+                    if(cam.position.y >= 500){
+                        if(deltaY < 0){
+                            cam.position.add(
+                                    cam.unproject(new Vector3(0, 0, 0))
+                                            .add(cam.unproject(new Vector3(deltaX, deltaY, 0)).scl(-1f))
+                            );
+                        }else{
+                            cam.position.add(
+                                    cam.unproject(new Vector3(0, 0, 0))
+                                            .add(cam.unproject(new Vector3(deltaX, 0, 0)).scl(-1f))
+                            );
+                        }
+                    }
                 }
 
 
                 return true;
             }
-
             @Override
-            public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
-                return false;
+            public boolean pinch (Vector2 initialFirstPointer, Vector2 initialSecondPointer, Vector2 firstPointer, Vector2 secondPointer){
+                if(!(initialFirstPointer.equals(oldInitialFirstPointer)&&initialSecondPointer.equals(oldInitialSecondPointer))){
+                    oldInitialFirstPointer = initialFirstPointer.cpy();
+                    oldInitialSecondPointer = initialSecondPointer.cpy();
+                    oldScale = cam.zoom;
+                }
+                Vector3 center = new Vector3(
+                        (firstPointer.x+initialSecondPointer.x)/2,
+                        (firstPointer.y+initialSecondPointer.y)/2,
+                        0
+                );
+                zoomCamera(center, oldScale*initialFirstPointer.dst(initialSecondPointer)/firstPointer.dst(secondPointer));
+                return true;
             }
-
-            @Override
-            public void pinchStop() {
-
-
+            private void zoomCamera(Vector3 origin, float scale){
+                cam.update();
+                Vector3 oldUnprojection = cam.unproject(origin.cpy()).cpy();
+                cam.zoom = scale; //Larger value of zoom = small images, border view
+                cam.zoom = Math.min(2.0f, Math.max(cam.zoom, 0.5f));
+                cam.update();
+                Vector3 newUnprojection = cam.unproject(origin.cpy()).cpy();
+                cam.position.add(oldUnprojection.cpy().add(newUnprojection.cpy().scl(-1f)));
             }
-        };
+        });
 
         stage.addListener(new InputListener() {
             public boolean scrolled(InputEvent event, float x, float y, int amount){
                 System.out.printf("Zoom:%f\n", cam.zoom);
-                if(cam.zoom >= 0.5f && cam.zoom <= 1f){
+                if(cam.zoom > 0.5f && cam.zoom < 1f){
                         cam.zoom += 0.05*amount;
                 }
                 else{
@@ -107,11 +245,10 @@ public class GameboardActivity extends Activity {
         });
 
 
-
         //Gdx.input.setInputProcessor(new GestureDetector(gestureListener));
         InputMultiplexer multiplexer = new InputMultiplexer();
         Gdx.input.setInputProcessor(multiplexer);
-        multiplexer.addProcessor(new GestureDetector(gestureListener));
+        multiplexer.addProcessor(gestureDetector);
         multiplexer.addProcessor(stage);
 
     }
