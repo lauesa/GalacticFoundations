deeef3d011267e1cb1fd7254e87afe4b1b1e6b9c
Kevin Williams <Kmwilliams323@gmail.com>
2017-11-30 08:30:31 -0600

    Added functionality to AI

--- core/src/com/github/t7/galacticfoundations/activities/AI_Activity.java
+++ core/src/com/github/t7/galacticfoundations/activities/AI_Activity.java
@@ -7,6 +7,7 @@ import com.github.t7.galacticfoundations.actors.Hex;
 import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by Borzantag on 11/25/2017.
@@ -15,6 +16,7 @@ import java.util.ArrayList;
 public class AI_Activity {
     public static int resources = 0;    //Count of resourcesa
     public static Array<Hex> Tiles;     //Array of Hexes that make up the board
+    public static Random rand;          //Instantiation of random class
 
     public static void AI_turn(Array<Hex> a){   //Function that drives AI turn
         Tiles = a;                              //Initialize Tiles Array with Hex values
@@ -22,7 +24,7 @@ public class AI_Activity {
         int tileLocations[][] = new int[11][9];                 //List of tile IDs
         int count = 1;                                         //Count for making sure each row gets a different weight
         int weight = 1;                                         //Initial weight, updated for each row
-
+        /*
         for(int i = 0; i < Tiles.size; i++) {                   //Loop to go through each Hex tile and initialize weight and
             tileLocations[weight-1][count-1] = i;               //Initializing ID of each tile
             if(Tiles.get(i).toString().split(" ")[0].equals("BASE") || Tiles.get(i).toString().split(" ")[0].equals("SPECIAL")){ //if a tile is a base or special tile add 5 to weight
@@ -43,7 +45,7 @@ public class AI_Activity {
             count++;                                                        //Increment count
         }
         Collections.reverse(tileWeights);                               //Reverse weights so that they start increasing from AI side of board
-
+        */
         while(resources != 0){                                          //Evaluate board and take action while AI has resources
             eval_board(tileWeights, tileLocations);
         }
@@ -61,12 +63,50 @@ public class AI_Activity {
             }
         }
 
-        int[][] adjacencyMatrix = hexAdjacent();
-        /*
-        while(AI_Tiles.size() >= 0){
+        int[][] adjacencyMatrix = hexAdjacent();                            //Gets adjacency matrix from function
+        ArrayList<Integer> Adj_Tiles = new ArrayList<Integer>();                 //ArrayList to hold all Tiles adjacent to a tile
+        ArrayList<Integer> Max_ID = new ArrayList<Integer>();                   //ArrayList to hold all forward moving Tiles adjacent to a tile
+        int max = 0;                                                        //Max variable for use in determining forward-most tiles
+        while(AI_Tiles.size() >= 0 || resources != 0){                                        //Loop will continue until out of tiles to use or out of resources
+            for(int i = 0; i < 95; i++){                                                      //Loop to go through and find adjacent tiles to first tile in AI_Tiles
+                if(adjacencyMatrix[AI_Tiles.get(0)][i] == 1){                                  //Checks if an ID for a tile is adjacent
+                    Adj_Tiles.add(adjacencyMatrix[AI_Tiles.get(0)][1]);                        //If so, add tile to Adjacency ArrayList
+                }
+            }
 
+            while(Max_ID.size() < 3){                                      //Loop that will continue to run until forwardmost half of adjacent tiles are added to ArrayList
+                for(int i = 0; i < Adj_Tiles.size(); i++){                  //Loop to go through Adjacent Tiles and find the max ID
+                    if(Adj_Tiles.get(i) > max){                             //Checks if current tile has a greater ID than the max ID
+                        max = Adj_Tiles.get(i);                             //If so, sets current max to current tile's ID
+                    }
+                }
+                Max_ID.add(max);                                        //Adds max value to array of Max IDs
+                Adj_Tiles.remove(Adj_Tiles.indexOf(max));               //Removes max value from Array so that secondary or tertiary max ID can be found
+                max = 0;                                                //Sets max back to zero
+            }
+
+            int count = 0;                                              //Count variable to count enemy tiles in forward positions
+            ArrayList<Integer> EnemyTiles = new ArrayList<Integer>();   //ArrayList to hold IDs of enemy tiles
+            for(int i = 0; i < Max_ID.size(); i++){                     //Loop to go through forward tiles and search for enemy tiles
+                if(Tiles.get(Max_ID.get(i)).toString().split(" ")[1].equals("PLAYER_ACTIVE")){  //Checks if a tile belongs to a player
+                    count++;                                                                        //If yes, increments count
+                    EnemyTiles.add(Max_ID.get(i));                                                  //Also adds the enemy tile's ID to ArrayList
+                }
+            }
+
+            if(count >= 2){                                                         //If count of enemy tiles greater than 2, make AoE attack
+                AOE_Attack(Tiles.get(AI_Tiles.get(0)), Adj_Tiles, Max_ID);            //Passes tile attack origin and adjacency matrix to AoE attack function
+            }
+            if(count == 1){                                                         //If count of enemy tiles is 1, make line attack
+                lineAttack(Tiles.get(AI_Tiles.get(0)), Tiles.get(EnemyTiles.get(0)));     //Passes tile attack origin and target
+            }
+            if(count == 0){                                                     //If no enemy tiles, expand
+                int randTile = randomTile(Max_ID);                              //Gets a random tile from forward most tiles to expand to
+                expand(Tiles.get(randTile));                                    //Expands to target tile
+            }
+            AI_Tiles.remove(0);                                             //Removes tile just used from ArrayList
         }
-        */
+
     }
 
     /*
@@ -78,22 +118,41 @@ public class AI_Activity {
     }
 
     /*
-    Method to make a line attack
-
+        Method to make a line attack
+        Input:Hex tile to originate attack from and Hex tile to target attack towards
+        Output:None
      */
-    public static void lineAttack(){
+    public static void lineAttack(Hex origin, Hex target){
     }
 
-    public static void AOE_Attack(){
+    /*
+        Method to make an AoE attack
+        Input: Hex tile that is origin of AoE attack and adjacentTiles from two different ArrayLists
+        Output: none
+     */
+    public static void AOE_Attack(Hex hex, ArrayList<Integer> adjacentTiles, ArrayList<Integer> Max){
+
     }
 
     public static void fortify(){
     }
 
-    public static void expand(){
+    public static void expand(Hex target){
+
     }
 
     public static void stockpile(){
+    }
+
+    /*
+        Function to take in a list of tiles and return a random tile ID
+        Input: ArrayList of Ints corresponding to tile IDs
+        Output: Random integer corresponding to a tile ID
+     */
+    public static int randomTile(ArrayList<Integer> ActionTiles){
+         rand = new Random();
+         int randTile = ActionTiles.get(rand.nextInt(ActionTiles.size()));
+         return randTile;
     }
 
     /*
@@ -107,7 +166,7 @@ public class AI_Activity {
             for(int i =0; i < 95; i++){                 //Go through every tile
                 for(int j =0; j<95; j++){               //Go through every possible tile connected to i
                     if(i == 0){                         //Checks if i is bottom left corner
-                        if(j == 9 || j == 2){           //If yes, add adjacency
+                        if(j == 5 || j == 9){           //If yes, add adjacency
                             adjacencyMatrix[i][j] = 1;
                         }
                         else {
@@ -115,15 +174,15 @@ public class AI_Activity {
                         }
                     }
                     if(i == 90){                        //Checks if i is top left corner
-                        if(j == 81 || j == 82){         //If yes, adds adjacency
+                        if(j == 81 || j == 86){         //If yes, adds adjacency
                             adjacencyMatrix[i][j] = 1;
                         }
                         else {
                             adjacencyMatrix[i][j] = 0;
                         }
                     }
-                    if(i==8){                           //Checks if i is bottom right corner
-                        if(j == 7 || j == 17){          //If yes, creates adjacency
+                    if(i==4){                           //Checks if i is bottom right corner
+                        if(j == 8 || j == 13){          //If yes, creates adjacency
                             adjacencyMatrix[i][j] = 1;
                         }
                         else {
@@ -131,7 +190,7 @@ public class AI_Activity {
                         }
                     }
                     if(i == 94){                        //Checks if i is top right corner
-                        if(j == 88 || j == 89){         //If yes, creates adjacency
+                        if(j == 85 || j == 89){         //If yes, creates adjacency
                             adjacencyMatrix[i][j] = 1;
                         }
                         else {
@@ -139,7 +198,7 @@ public class AI_Activity {
                         }
                     }
                     if(i!= 0 && i!= 90 && i%9 == 0){            //Checks if i is on left edge
-                        if(j == (i+9) || j== (i+1) || j == (i-9) || j == (i-8) ){   //If yes, add adjacency
+                        if(j == (i+9) || j== (i+5) || j == (i-4) || j == (i-9) ){   //If yes, add adjacency
                             adjacencyMatrix[i][j] = 1;
                         }
                         else{
@@ -147,7 +206,7 @@ public class AI_Activity {
                         }
                     }
                     if(i!=8 && i!=94 && i%9 == 8 ){             //Checks if i is on right edge
-                        if(j == (i+9) || j == (i-9) || j == (i-1) || j == (i-10)){      //If yes, add adjacency
+                        if(j == (i+9) || j == (i-9) || j == (i-5) || j == (i+4)){      //If yes, add adjacency
                             adjacencyMatrix[i][j] = 1;
                         }
                         else{
@@ -155,7 +214,7 @@ public class AI_Activity {
                         }
                     }
                     if(i <= 7 && i>= 1 && i%2 == 0){            //Checks if i is on bottom edge and even
-                        if(j == (i-1) || j == (i+1) || j == (i+9)){             //If yes, add adjacency
+                        if(j == (i+4) || j == (i+5) || j == (i+9)){             //If yes, add adjacency
                             adjacencyMatrix[i][j] = 1;
                         }
                         else{
@@ -163,23 +222,39 @@ public class AI_Activity {
                         }
                     }
                     if(i <=7 && i >=1 && i%2 == 1){             //Checks if i is on bottom edge and odd
-                        if(j == (i-1) || j == (i+1) || j == (i+9) || j == (i+10) || j == (j+8)){    //If yes, add adjacency
+                        if(j == (i-4) || j == (i-5) || j == (i+9) || j == (i+4) || j == (j+5)){    //If yes, add adjacency
                             adjacencyMatrix[i][j] = 1;
                         }
                         else{
                             adjacencyMatrix[i][j] = 0;
                         }
                     }
-                    if(i!=0 && i!= 8 && i != 90 && i!= 94 && i > 7 && i%9 != 0 && i%9 != 8 && i%2 == 1) {   //Checks if i is non-edge tile and odd
-                        if (j == (i + 9) || j == (i + 8) || j == (i + 10) || j == (i - 9) || j == (i - 1) || j == (i + 1)) {    //If yes, add adjacency
+                    if(i <= 94 && i >= 90 && i!= 90 && i!= 94){ //Checks if i is top outermost edge
+                        if(j == (i-9) || j == (i - 4) || j == (i-5)){ //If yes, add adjacency
                             adjacencyMatrix[i][j] = 1;
                         }
                         else{
                             adjacencyMatrix[i][j] = 0;
                         }
                     }
-                    if(i!=0 && i!= 8 && i != 90 && i != 94 && i > 7 && i%9 != 0 && i%9 != 8 && i%2 ==0){    //Checks if i is non-edge tile and even
-                        if(j == (i-9) || j == (i-8) || j == (i-10) || j == (i-1) || j == (i + 1) || j == (i+9)){    //If yes, add adjacency
+                    if(i <= 89 && i >= 86){                         //Checks if i is top innermost edge
+                        if(j == (i-9) || j == (i+4) || j == (i+5)|| j == (i-4) || j== (i-5)){   //If yes, add adjacency
+                            adjacencyMatrix[i][j] = 1;
+                        }
+                        else{
+                            adjacencyMatrix[i][j] = 0;
+                        }
+                    }
+                    if(i!=0 && i!= 8 && i != 90 && i!= 94 && i > 7 && i%9 != 0 && i%9 != 8 && i < 86 && i%2 == 1) {   //Checks if i is non-edge tile and odd
+                        if (j == (i + 9) || j == (i + 4) || j == (i + 5) || j == (i - 9) || j == (i - 4) || j == (i - 5)) {    //If yes, add adjacency
+                            adjacencyMatrix[i][j] = 1;
+                        }
+                        else{
+                            adjacencyMatrix[i][j] = 0;
+                        }
+                    }
+                    if(i!=0 && i!= 8 && i != 90 && i != 94 && i > 7 && i%9 != 0 && i%9 != 8 && i < 86 && i%2 ==0){    //Checks if i is non-edge tile and even
+                        if(j == (i-9) || j == (i-4) || j == (i-5) || j == (i+4) || j == (i + 5) || j == (i+9)){    //If yes, add adjacency
                             adjacencyMatrix[i][j] = 1;
                         }
                         else{
